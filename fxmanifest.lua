61d5efb45fc16f7eae3d7d71274c6393b470d368
SundarBau <sundarsunuwarjr@gmail.com>
2025-06-29 08:10:16 +0545

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'stream/*.ydd',
+    'stream/*.ytd',
+    'stream/*.ymt',
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/*.ymt'
+data_file 'PED_COMPONENT_TEXTURE_FILE' 'stream/*.ytd'
+data_file 'PED_COMPONENT_META_FILE' 'stream/*.ydd'
